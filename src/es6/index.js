cfc45dfc53eb27bf0e59d81e6471e329ffc05c87
Israel Yance <israelyance@gmail.com>
2020-06-23 19:46:51 -0500

    es6-2

--- src/es6/index.js
+++ src/es6/index.js
@@ -5,6 +5,7 @@ function newFunction(name, age, country) {
     console.log(name, age, country)
 }
 
+
 // es6
 function newFunction2 (name = 'oscar', age = 32, country = 'MX') {
     console.log(name, age, country)
@@ -13,6 +14,7 @@ function newFunction2 (name = 'oscar', age = 32, country = 'MX') {
 newFunction2()
 newFunction2('Ricardo', '23', 'CO')
 
+
 //
 let hello = 'Hello'
 let world = 'World'
@@ -20,3 +22,58 @@ let epicPhrase = hello + ' ' + world
 console.log(epicPhrase)
 let epicPhrase2 = `{hello} {world}`
 console.log(epicPhrase)
+
+
+//
+let lorem = "Lorem Ipsum is simply dummy text of the \n"
++ "printing and typesetting industry."
+
+let lorem2 = `Lorem Ipsum is simply dummy text of the
+printing and typesetting industry.`
+
+console.log(lorem)
+console.log(lorem2)
+
+
+//
+let person = {
+    'name': 'oscar',
+    'age': 32,
+    'country': 'MX'
+}
+
+console.log(person.name, person.age)
+
+let {age, country} = person
+console.log(age, country)
+
+
+//
+let team1 = ['Oscar', 'Julian', 'Ricardo']
+let team2 = ['Valeria', 'Yesica', 'Camila']
+
+let education = ['David', ...team1, ...team2]
+
+console.log(education)
+
+
+//
+{
+    var globalVar = 'Global Var'
+}
+
+{
+    let globalLet = 'Global Let'
+    console.log(globalLet)
+}
+
+console.log(globalVar)
+// console.log(globalLet) // error
+
+
+//
+var a = 'b'
+a = 'a'
+
+const a = 'b'
+a = 'a'
