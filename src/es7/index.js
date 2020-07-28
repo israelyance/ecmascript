52872642157126bb6faad43a5cd111286dc1207a
Israel Yance <israelyance@gmail.com>
2020-07-28 01:56:21 -0500

    es7-1

---
+++ src/es7/index.js
@@ -0,0 +1,18 @@
+// Includes
+//////
+let numbers = [1, 2, 3, 7, 8]
+
+if (numbers.includes(9)) {
+    console.log('Sí se encuentra en valor 7')
+} else {
+    console.log('No se encuentra')
+}
+
+
+// Potencia
+//////
+let base = 4
+let exponent = 3
+let result = base ** exponent
+
+console.log(result)
